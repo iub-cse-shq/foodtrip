5c5ad1fc2c389ba3b982741513d20254f53c9032
Shama Hoque <shama.hoque@gmail.com>
2017-06-13 03:54:40 +0000

    add homepage&placepaGE

--- controllers/core.server.controller.js
+++ controllers/core.server.controller.js
@@ -4,12 +4,17 @@
  * Module dependencies.
  */
 exports.index = function(req, res) {
-	res.render('./../index.ejs', {
+	res.render('./../public/views/project.ejs', {
 		user: req.user || null,
 		request: req
 	});
 };
-
+exports.place = function(req, res) {
+	res.render('./../public/views/place.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
 exports.about = function(req, res) {
 	res.render('./../about.ejs', {
 		user: req.user || null,
